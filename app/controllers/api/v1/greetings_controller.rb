ef8110244120339b303ebf9e786461aed9dd5127
Olaoye Abayomi Oluwatoyin <mindprepopps@gmail.com>
2023-05-17 15:28:28 +0100

    Fix syntax and linting issues

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,6 @@
 class GreetingsController < ApplicationController
   def random_greeting
-    @greeting = Message.order("RANDOM()").first
+    @greeting = Message.order('RANDOM()').first
     render json: { greeting: @greeting.greeting }
   end
 end
